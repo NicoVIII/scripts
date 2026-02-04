68289818a41dfc3242e0bb945ee2e831f21733bb
NicoVIII <nico@nicoviii.net>
2026-02-04 19:14:52 +0100

    Update mka_to_opus.sh

--- audiobooks/mka_to_opus.sh
+++ audiobooks/mka_to_opus.sh
@@ -2,13 +2,13 @@
 set -e
 
 # This script is used to convert a .mka file to a smaller .mka file with libopus codec inside
-# The bitrate defaults to 32k which is ideal for podcasts / audiobooks
+# I use 32k for podcasts / audiobooks and 48k for radio plays
 # Have a look at https://wiki.xiph.org/Opus_Recommended_Settings for recommended settings
 # for different types of audio
-[ "$#" -ge 1 ] && [ "$#" -le 2 ] || (echo "Usage: ./mka_to_opus.sh <input_file> [bit_rate]" && exit 1)
+[ "$#" -eq 2 ] || (echo "Usage: ./mka_to_opus.sh <input_file> <bit_rate>" && exit 1)
 
 input_file="$1"
-bitrate="${2:-48}"
+bitrate="$2"
 
 filename=$(basename --suffix=".mka" "$input_file")
 output_file="$filename.opus"
