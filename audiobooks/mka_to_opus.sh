c6bc83fae8ce27b715646e1089ee10d0afdfcac8
NicoVIII <nico@nicoviii.net>
2026-02-12 13:14:20 +0100

    Update mka_to_opus.sh

--- audiobooks/mka_to_opus.sh
+++ audiobooks/mka_to_opus.sh
@@ -5,7 +5,11 @@ set -e
 # I use 32k for podcasts / audiobooks and 48k for radio plays
 # Have a look at https://wiki.xiph.org/Opus_Recommended_Settings for recommended settings
 # for different types of audio
-[ "$#" -eq 2 ] || (echo "Usage: ./mka_to_opus.sh <input_file> <bit_rate>" && exit 1)
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./mka_to_opus.sh <input_file> <bit_rate>"
+  exit 1
+fi
 
 input_file="$1"
 bitrate="$2"
